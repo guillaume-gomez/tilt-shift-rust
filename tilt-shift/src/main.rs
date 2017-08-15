6590197ff1bba384df63a3d41bb998c5098737ca
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-08-15 11:47:48 +0200

    test how to flip and resize image

--- tilt-shift/src/main.rs
+++ tilt-shift/src/main.rs
@@ -5,7 +5,10 @@ use std::env;
 use std::fs::File;
 use std::path::Path;
 
-use image::GenericImage;
+use image::{
+    GenericImage,
+    FilterType
+};
 
 fn main() {
     let file = if env::args().count() == 2 {
@@ -16,16 +19,17 @@ fn main() {
 
     // Use the open function to load an image from a PAth.
     // ```open``` returns a dynamic image.
-    let im = image::open(&Path::new(&file)).unwrap();
+    let img = image::open(&Path::new(&file)).unwrap();
 
     // The dimensions method returns the images width and height
-    println!("dimensions {:?}", im.dimensions());
+    println!("dimensions {:?}", img.dimensions());
 
     // The color method returns the image's ColorType
-    println!("{:?}", im.color());
+    println!("{:?}", img.color());
 
+    let filtered = img.fliph().resize(300,300, FilterType::Nearest);
     let fout = &mut File::create(&Path::new(&format!("{}.png", file))).unwrap();
 
     // Write the contents of this image to the Writer in PNG format.
-    im.save(fout, image::PNG).unwrap();
+    filtered.save(fout, image::PNG).unwrap();
 }
