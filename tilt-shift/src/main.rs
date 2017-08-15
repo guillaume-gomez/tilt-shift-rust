fb7658f720e1d936a1f93bd95049672979d77956
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-08-16 00:24:57 +0200

    wip to create mask

--- tilt-shift/src/main.rs
+++ tilt-shift/src/main.rs
@@ -7,8 +7,25 @@ use std::path::Path;
 
 use image::{
     GenericImage,
-    FilterType
+    FilterType,
+    ImageBuffer,
+    Pixel,
+    Rgb
 };
+
+
+// wip shoud understand why rust in soooooo complex about return function
+fn create_mask(width: u32, height: u32, x_rect: u32, y_rect: u32, width_rect: u32, height_rect: u32) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+    let img_created = ImageBuffer::from_fn(width, height, |x, y| {
+
+        if x >= x_rect && x <= x_rect + width_rect && y >= y_rect && y <= y_rect + height_rect {
+            image::Luma([0u8])
+        } else {
+            image::Luma([255u8])
+        }
+    });
+    img_created;
+}
 
 fn main() {
     let file = if env::args().count() == 2 {
@@ -32,4 +49,8 @@ fn main() {
 
     // Write the contents of this image to the Writer in PNG format.
     filtered.save(fout, image::PNG).unwrap();
+
+    let img_created = create_mask(400,400, 5, 20, 30, 50);
+    let path = &Path::new("temp.png");
+    //Write the contents of this image to the Writer in PNG format.
 }
