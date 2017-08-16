47b98ef285e18a493091cbc20bcb8000a97eb125
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-08-16 23:10:38 +0200

    partially done the algorithm

--- tilt-shift/src/main.rs
+++ tilt-shift/src/main.rs
@@ -41,13 +41,28 @@ fn main() {
     // The color method returns the image's ColorType
     println!("{:?}", img.color());
 
-    let filtered = img.fliph().resize(300,300, FilterType::Nearest);
+    let filtered = img.fliph().resize_exact(600, 600, FilterType::Nearest);
     let fout = &mut File::create(&Path::new(&format!("{}.png", file))).unwrap();
 
     // Write the contents of this image to the Writer in PNG format.
     filtered.save(fout, image::PNG).unwrap();
 
-    let img_created = create_mask(500,400, 100, 20, 30, 50);
-    let path = &Path::new("temp.png");
-    img_created.save(path);
+    let mask = create_mask(600, 600, 0, 200, 600, 200);
+    let path = &Path::new("mask.png");
+    mask.save(path).unwrap();
+
+    let mask_blurred = image::open(path).unwrap().blur(10.0);
+    let path_2 = &Path::new("mask_blurred.png");
+    let fout2 = &mut File::create(path_2).unwrap();
+    mask_blurred.save(fout2, image::PNG).unwrap();
+
+    let blend_image = ImageBuffer::from_fn(600, 600, |x, y| {
+        let pixel_image = filtered.get_pixel(x, y);
+        let pixel_mask = mask_blurred.get_pixel(x, y);
+        image::Rgba([pixel_image.data[0], pixel_image.data[1], pixel_image.data[2], 255 - pixel_mask.data[0]])
+    });
+    let path_3 = &Path::new("image_mask_blurred.png");
+    blend_image.save(path_3).unwrap();
+
+
 }
