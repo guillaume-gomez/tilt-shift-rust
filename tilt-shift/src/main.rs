05f6ca57b78b808fe910e9694687f8327ba03083
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-08-16 20:02:02 +0200

    change return type

--- tilt-shift/src/main.rs
+++ tilt-shift/src/main.rs
@@ -8,14 +8,13 @@ use std::path::Path;
 use image::{
     GenericImage,
     FilterType,
-    ImageBuffer
+    ImageBuffer,
+    GrayImage
 };
 
 
-// wip shoud understand why rust in soooooo complex about return function
-fn create_mask (width: u32, height: u32, x_rect: u32, y_rect: u32, width_rect: u32, height_rect: u32) -> ImageBuffer<image::Luma<u8>, std::vec::Vec<u8>> {
+fn create_mask (width: u32, height: u32, x_rect: u32, y_rect: u32, width_rect: u32, height_rect: u32) -> GrayImage {
     let img_created = ImageBuffer::from_fn(width, height, |x, y| {
-
         if x >= x_rect && x <= x_rect + width_rect && y >= y_rect && y <= y_rect + height_rect {
             image::Luma([0u8])
         } else {
@@ -48,8 +47,7 @@ fn main() {
     // Write the contents of this image to the Writer in PNG format.
     filtered.save(fout, image::PNG).unwrap();
 
-    let img_created = create_mask(140,400, 5, 20, 30, 50);
-    let path = &Path::new("tempbite.png");
+    let img_created = create_mask(500,400, 100, 20, 30, 50);
+    let path = &Path::new("temp.png");
     img_created.save(path);
-    //Write the contents of this image to the Writer in PNG format.
 }
