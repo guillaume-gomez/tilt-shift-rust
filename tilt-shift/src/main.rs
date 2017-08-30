89c4e560c7126fae647a5fb91791b7baf44c6598
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-08-30 14:51:11 +0200

    blend two images to make the blur smooth

--- tilt-shift/src/main.rs
+++ tilt-shift/src/main.rs
@@ -9,7 +9,8 @@ use image::{
     GenericImage,
     FilterType,
     ImageBuffer,
-    GrayImage
+    GrayImage,
+    Pixel
 };
 
 
@@ -57,7 +58,7 @@ fn main() {
     //let fout2 = &mut File::create(path_2).unwrap();
     //mask_blurred.save(fout2, image::PNG).unwrap();
     println!("start blend_image ");
-    let blend_image = ImageBuffer::from_fn(600, 600, |x, y| {
+    let mut blend_image = ImageBuffer::from_fn(600, 600, |x, y| {
         let pixel_image = filtered.get_pixel(x, y);
         let pixel_mask = mask_blurred.get_pixel(x, y);
         image::Rgba([pixel_image.data[0], pixel_image.data[1], pixel_image.data[2], 255 - pixel_mask.data[0]])
@@ -67,15 +68,13 @@ fn main() {
     blend_image.save(path_3).unwrap();
 
     let filtered_blurred = filtered.blur(2.5);
-    let final_image_without_saturation = ImageBuffer::from_fn(600, 600, |x, y| {
-        let pixel_target = blend_image.get_pixel(x, y);
-        let pixel_source = filtered_blurred.get_pixel(x, y);
-        if pixel_target[3] != 0 {
-            image::Rgb([pixel_target.data[0], pixel_target.data[1], pixel_target.data[2]])
-        } else {
-            image::Rgb([pixel_source.data[0], pixel_source.data[1], pixel_source.data[2]])
-        }
-    });
+    let mut final_image_without_saturation = image::ImageBuffer::new(600, 600);
+    for(x, y, pixel) in final_image_without_saturation.enumerate_pixels_mut() {
+        let pixel_target = blend_image.get_pixel_mut(x, y);
+        let mut pixel_source = filtered_blurred.get_pixel(x, y);
+        pixel_source.blend(&pixel_target);
+        *pixel = image::Rgba([pixel_source.data[0], pixel_source.data[1], pixel_source.data[2], pixel_source.data[3]])
+    }
     final_image_without_saturation.save(&Path::new("result.png")).unwrap();
 
 
