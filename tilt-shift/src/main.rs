86a54eadaeb026cdf1440c2ecefa1ed21cdc768d
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-09-25 19:35:35 +0200

    add function

--- tilt-shift/src/main.rs
+++ tilt-shift/src/main.rs
@@ -25,6 +25,15 @@ fn create_mask (width: u32, height: u32, x_rect: u32, y_rect: u32, width_rect: u
     img_created
 }
 
+fn blended_image(width: u32, height: u32, original: &image::DynamicImage, mask: GrayImage) -> image::ImageBuffer<image::Rgba<u8>, std::vec::Vec<u8>> {
+    let blended_image = ImageBuffer::from_fn(width, height, |x, y| {
+        let pixel_image = original.get_pixel(x, y);
+        let pixel_mask = mask.get_pixel(x, y);
+        image::Rgba([pixel_image.data[0], pixel_image.data[1], pixel_image.data[2], 255 - pixel_mask.data[0]])
+    });
+    blended_image
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -51,17 +60,12 @@ fn main() {
     let filtered = img.resize_exact(width, height, FilterType::Nearest);
 
     let mask = create_mask(width, height, 0, 150, height, 350);
-
-    let blend_image = ImageBuffer::from_fn(width, height, |x, y| {
-        let pixel_image = filtered.get_pixel(x, y);
-        let pixel_mask = mask.get_pixel(x, y);
-        image::Rgba([pixel_image.data[0], pixel_image.data[1], pixel_image.data[2], 255 - pixel_mask.data[0]])
-    });
+    let blended_image = blended_image(width, height, &filtered, mask);
 
     let filtered_blurred = filtered.blur(blur);
     let mut final_image_without_saturation_buff = image::ImageBuffer::new(width, height);
     for(x, y, pixel) in final_image_without_saturation_buff.enumerate_pixels_mut() {
-        let pixel_target = blend_image.get_pixel(x, y);
+        let pixel_target = blended_image.get_pixel(x, y);
         let mut pixel_source = filtered_blurred.get_pixel(x, y);
         pixel_source.blend(&pixel_target);
         *pixel = image::Rgba(pixel_source.data)
