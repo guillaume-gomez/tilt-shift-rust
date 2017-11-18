f0a6b0a4bd1221c8b5ed26cb8a246045290744ca
guillaume-gomez <guillaume.gomez846@gmail.com>
2017-11-19 00:29:56 +0100

    use it in main function

--- src/main.rs
+++ src/main.rs
@@ -7,55 +7,10 @@ use std::fs::File;
 use std::path::Path;
 
 use image::{
-    GenericImage,
-    ImageBuffer,
-    GrayImage,
-    Pixel
+    GenericImage
 };
 
-
-fn create_mask(width: u32, height: u32, x_rect: u32, y_rect: u32, width_rect: u32, height_rect: u32) -> GrayImage {
-    let img_created = ImageBuffer::from_fn(width, height, |x, y| {
-        if x >= x_rect && x <= x_rect + width_rect && y >= y_rect && y <= y_rect + height_rect {
-            image::Luma([0u8])
-        } else {
-            image::Luma([255u8])
-        }
-    });
-    img_created
-}
-
-fn blended_image(width: u32, height: u32, original: &image::DynamicImage, mask: GrayImage) -> image::ImageBuffer<image::Rgba<u8>, std::vec::Vec<u8>> {
-    let blended_image = ImageBuffer::from_fn(width, height, |x, y| {
-        let pixel_image = original.get_pixel(x, y);
-        let pixel_mask = mask.get_pixel(x, y);
-        image::Rgba([pixel_image.data[0], pixel_image.data[1], pixel_image.data[2], 255 - pixel_mask.data[0]])
-    });
-    blended_image
-}
-
-fn mix_from_blurred_and_blended_image(width: u32, height: u32, blurred_image: image::DynamicImage, blended_image: image::ImageBuffer<image::Rgba<u8>, std::vec::Vec<u8>> ) -> image::ImageBuffer<image::Rgba<u8>, std::vec::Vec<u8>> {
-    let mut final_image_without_saturation_buff = image::ImageBuffer::new(width, height);
-    for(x, y, pixel) in final_image_without_saturation_buff.enumerate_pixels_mut() {
-        let pixel_target = blended_image.get_pixel(x, y);
-        let mut pixel_source = blurred_image.get_pixel(x, y);
-        pixel_source.blend(&pixel_target);
-        *pixel = image::Rgba(pixel_source.data)
-    }
-    final_image_without_saturation_buff
-}
-
-fn tilt_shift_algorithm(original_image: &image::DynamicImage, y_point_of_interest :u32, height_point_of_interest :u32, blur :f32, contrast :f32) -> image::DynamicImage {
-    let (width, height) = original_image.dimensions();
-    let mask = create_mask(width, height, 0, y_point_of_interest, width, height_point_of_interest);
-    let blended_image = blended_image(width, height, &original_image, mask);
-
-    let filtered_blurred = original_image.blur(blur);
-    let final_image_without_saturation_buff = mix_from_blurred_and_blended_image(width, height, filtered_blurred, blended_image);
-
-    return image::ImageRgba8(final_image_without_saturation_buff).adjust_contrast(contrast);
-}
-
+mod tilt_shift_module;
 
 fn create_single_image(matches: clap::ArgMatches) {
     let file = matches.value_of("filename").unwrap();
@@ -80,7 +35,7 @@ fn create_single_image(matches: clap::ArgMatches) {
         height / 3
     };
 
-    let final_image = tilt_shift_algorithm(&img, y_point_of_interest, height_point_of_interest, blur, contrast);
+    let final_image = tilt_shift_module::tilt_shift_algorithm(&img, y_point_of_interest, height_point_of_interest, blur, contrast);
     let path_final_result = &Path::new(output_file);
     let fout_final = &mut File::create(path_final_result).unwrap();
     final_image.save(fout_final, image::PNG).unwrap();
@@ -111,7 +66,7 @@ fn create_several_images(matches: clap::ArgMatches) {
     let step = 1.0;
 
     while current_blur < blur_max {
-        let final_image = tilt_shift_algorithm(&img, y_point_of_interest, height_point_of_interest, current_blur, contrast);
+        let final_image = tilt_shift_module::tilt_shift_algorithm(&img, y_point_of_interest, height_point_of_interest, current_blur, contrast);
         let output_file = format!("{}_{}", matches.value_of("output_file_name").unwrap(), current_blur);
         let path_final_result = &Path::new(&output_file);
         let fout_final = &mut File::create(path_final_result).unwrap();
